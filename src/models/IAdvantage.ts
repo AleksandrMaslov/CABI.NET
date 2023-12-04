218cefec680b18f886416fbdd7aecbd4a52fa3c9
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-05 00:53:57 +0300

    add: Advantages model and service

---
+++ src/models/IAdvantage.ts
@@ -0,0 +1,17 @@
+export default interface IAdvantage {
+  icon:
+    | 'appliances'
+    | 'baby'
+    | 'briefcase'
+    | 'camera'
+    | 'chat'
+    | 'location'
+    | 'lock'
+    | 'mail'
+    | 'printer'
+    | 'trash'
+    | 'wifi'
+    | 'yoga'
+  title: string
+  content: string
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,1 +1,2 @@
+export type { default as IAdvantage } from './IAdvantage'
 export type { default as IObject } from './IObject'
