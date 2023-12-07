3509c3b4db6749c2e42c05be53b6840af7d14fda
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-07 20:38:01 +0300

    layout: HomePage Plan block

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,7 @@ import about_img from './about_img.webp'
 import contacts_map from './contacts_map.webp'
 import hero_bg from './hero_bg.webp'
 import hero_img from './hero_img.webp'
+import plan_bg from './plan_bg.webp'
 import request_bg from './request_bg.webp'
 import request_text_bg from './request_text_bg.webp'
 
@@ -10,6 +11,7 @@ export {
   contacts_map,
   hero_bg,
   hero_img,
+  plan_bg,
   request_bg,
   request_text_bg,
 }
--- src/components/business/HomePage/Plan/Plan.tsx
+++ src/components/business/HomePage/Plan/Plan.tsx
@@ -1,4 +1,9 @@
-import { FC } from 'react'
+import { Img, Marker } from 'cabinet_ui_kit'
+import { FC, useEffect, useState } from 'react'
+
+import { plan_bg } from 'src/assets'
+import { IMarkerData } from 'src/models'
+import { SpacesService } from 'src/services'
 
 import Breaker from '../Breaker/Breaker'
 
@@ -12,11 +17,24 @@ const Plan: FC<PlanProps> = ({ className }) => {
   const rootClasses = [classes.plan]
   if (className) rootClasses.push(className)
 
+  const [markers, setMarkers] = useState<IMarkerData[]>([])
+
+  useEffect(() => {
+    setMarkers(SpacesService.getMarkersData())
+  }, [])
+
   return (
     <section className={rootClasses.join(' ')}>
       <div className={classes.container}>
         <Breaker number="03" title="План коворкинга" />
-        Plan
+
+        <div className={classes.wrapper}>
+          <Img src={plan_bg} />
+
+          {markers.map((props: IMarkerData) => (
+            <Marker key={props.tooltip} {...props} />
+          ))}
+        </div>
       </div>
     </section>
   )
