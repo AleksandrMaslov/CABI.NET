67e04cf00f972339875271bf618784be9b6ecd7f
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-10 22:45:44 +0300

    add: Header sticky and scroll animation

--- src/components/business/HomePage/Header/Header.tsx
+++ src/components/business/HomePage/Header/Header.tsx
@@ -1,4 +1,5 @@
 import { Logo } from 'cabinet_ui_kit'
+import { motion, useScroll, useTransform } from 'framer-motion'
 import { FC } from 'react'
 
 import { Navbar } from '../..'
@@ -13,8 +14,11 @@ const Header: FC<HeaderProps> = ({ className }) => {
   const rootClasses = [classes.header]
   if (className) rootClasses.push(className)
 
+  const { scrollY } = useScroll()
+  const height = useTransform(scrollY, [100, 300], ['13rem', '10rem'])
+
   return (
-    <header className={rootClasses.join(' ')}>
+    <motion.header className={rootClasses.join(' ')} style={{ height }}>
       <div className={classes.overline} />
       <div className={classes.container}>
         <a href="#" className={classes.logo}>
@@ -23,7 +27,7 @@ const Header: FC<HeaderProps> = ({ className }) => {
 
         <Navbar />
       </div>
-    </header>
+    </motion.header>
   )
 }
 
