22d7a30abddefaa5f3cc84ff81b3d224e5d1c5d0
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-11 00:26:38 +0300

    add: Breaker animation

--- src/components/business/HomePage/Breaker/Breaker.tsx
+++ src/components/business/HomePage/Breaker/Breaker.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { FC } from 'react'
 
 import classes from './Breaker.module.css'
@@ -8,15 +9,43 @@ interface BreakerProps {
   className?: string
 }
 
+const lineVariants = {
+  hidden: { width: '0%' },
+
+  visible: {
+    width: '100%',
+    transition: { duration: 0.5, bounce: 0 },
+  },
+}
+
+const titleVariants = {
+  hidden: { opacity: 0 },
+
+  visible: {
+    opacity: 1,
+    transition: { delay: 0.5, bounce: 0 },
+  },
+}
+
 const Breaker: FC<BreakerProps> = ({ number, title, className }) => {
   const rootClasses = [classes.breaker]
   if (className) rootClasses.push(className)
 
   return (
-    <div className={rootClasses.join(' ')}>
-      <h4 className={classes.header}>{number}</h4>
-      <h4 className={classes.header}>{title}</h4>
-    </div>
+    <motion.div
+      className={rootClasses.join(' ')}
+      initial="hidden"
+      whileInView="visible"
+      variants={lineVariants}
+      viewport={{ once: true, amount: 0.3 }}
+    >
+      <motion.h4 className={classes.header} variants={titleVariants}>
+        {number}
+      </motion.h4>
+      <motion.h4 className={classes.header} variants={titleVariants}>
+        {title}
+      </motion.h4>
+    </motion.div>
   )
 }
 
