6f453d28f9effb1d4c66b0ecafbe62c8f35b9992
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-10 21:31:14 +0300

    add: spaces images

---
+++ src/assets/spaces/index.ts
@@ -0,0 +1,25 @@
+import lecture_lira from './lecture_lira.jpg'
+import meeting_andromeda from './meeting_andromeda.jpg'
+import meeting_orion from './meeting_orion.jpg'
+import office_alkor from './office_alkor.jpg'
+import office_kapella from './office_kapella.jpg'
+import office_meridiana from './office_meridiana.jpg'
+import office_mira from './office_mira.jpg'
+import office_persei from './office_persei.jpg'
+import office_sirius from './office_sirius.jpg'
+import office_vega from './office_vega.jpg'
+import workspace_kasiopea from './workspace_kasiopea.jpg'
+
+export {
+  lecture_lira,
+  meeting_andromeda,
+  meeting_orion,
+  office_alkor,
+  office_kapella,
+  office_meridiana,
+  office_mira,
+  office_persei,
+  office_sirius,
+  office_vega,
+  workspace_kasiopea,
+}
