9948655954e959365f5334bf7880d20705c1aa7c
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-10 23:52:02 +0300

    add: Hero block animation

--- src/components/business/HomePage/Cover/Cover.tsx
+++ src/components/business/HomePage/Cover/Cover.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import { FC } from 'react'
 
 import classes from './Cover.module.css'
@@ -6,39 +7,78 @@ interface CoverProps {
   className?: string
 }
 
+const variants = {
+  hidden: { opacity: 0, y: 200 },
+
+  visible: (delay: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: delay * 0.1, bounce: 0 },
+  }),
+}
+
 const Cover: FC<CoverProps> = ({ className }) => {
   const rootClasses = [classes.cover]
   if (className) rootClasses.push(className)
 
   return (
-    <div className={rootClasses.join(' ')}>
-      <h4 className={`${classes.lightgreen} ${classes.light}`}>
+    <motion.div
+      className={rootClasses.join(' ')}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0, once: true }}
+      variants={variants}
+    >
+      <motion.h4
+        className={`${classes.lightgreen} ${classes.light}`}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: 0, once: true }}
+        variants={variants}
+        custom={1}
+      >
         КОВОРКИНГ ЯВЛЯЕТСЯ ПРОВОДНИКОМ ПЕРЕМЕН И ОТВЕТОМ НА&nbsp;ОБЩЕСТВЕННЫЕ
         ПОТРЕБНОСТИ. ЭТО МЕСТО ГДЕ В ОДНОМ ОБЩЕМ ПРОСТРАНСТВЕ ОБЪЕДИНЯЮТСЯ ЛЮДИ
         С РАЗНОЙ ЗАНЯТОСТЬЮ ДЛЯ ОБЩЕНИЯ И&nbsp;ТВОРЧЕСКОГО ВЗАИМОДЕЙСТВИЯ
-      </h4>
+      </motion.h4>
 
-      <h3 className={`${classes.black} ${classes.centered} ${classes.light}`}>
+      <motion.h3
+        className={`${classes.black} ${classes.centered} ${classes.light}`}
+        variants={variants}
+        custom={4}
+      >
         435м2
-      </h3>
+      </motion.h3>
 
-      <div className={`${classes.orange} ${classes.centered}`}>
+      <motion.div
+        className={`${classes.orange} ${classes.centered}`}
+        variants={variants}
+        custom={6}
+      >
         <h3 className={classes.light}>7</h3>
         <p className={`${classes.content} ${classes.light}`}>смарт-офисов</p>
-      </div>
+      </motion.div>
 
-      <div className={`${classes.blue} ${classes.centered}`}>
+      <motion.div
+        className={`${classes.blue} ${classes.centered}`}
+        variants={variants}
+        custom={8}
+      >
         <h3 className={classes.light}>2</h3>
         <p className={`${classes.content} ${classes.light}`}>переговорных</p>
-      </div>
+      </motion.div>
 
-      <div className={`${classes.green} ${classes.centered}`}>
+      <motion.div
+        className={`${classes.green} ${classes.centered}`}
+        variants={variants}
+        custom={10}
+      >
         <h3 className={classes.light}>20</h3>
         <p className={`${classes.content} ${classes.light}`}>
           рабочих мест в&nbsp;опенспейсах
         </p>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
 
--- src/components/business/HomePage/Hero/Hero.tsx
+++ src/components/business/HomePage/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import { Img } from 'cabinet_ui_kit'
+import { motion } from 'framer-motion'
 import { FC } from 'react'
 
 import { hero_img } from 'src/assets/home'
@@ -11,6 +12,16 @@ interface HeroProps {
   className?: string
 }
 
+const variants = {
+  hidden: { opacity: 0, x: -200 },
+
+  visible: (delay: number) => ({
+    opacity: 1,
+    x: 0,
+    transition: { delay: delay * 0.04, bounce: 0 },
+  }),
+}
+
 const Hero: FC<HeroProps> = ({ className }) => {
   const rootClasses = [classes.hero]
   if (className) rootClasses.push(className)
@@ -18,15 +29,21 @@ const Hero: FC<HeroProps> = ({ className }) => {
   return (
     <section className={rootClasses.join(' ')}>
       <div className={classes.container}>
-        <div className={classes.wrapper}>
+        <motion.div
+          className={classes.wrapper}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+        >
           <Img className={classes.img} src={hero_img} />
 
-          <Title className={classes.title} text="КОВОРКИНГ НА ЮГЕ МОСКВЫ" />
-
-          <h1 className={classes.header}>
-            САМЫЕ ЛУЧШИЕ КВАДРАТНЫЕ МЕТРЫ В&nbsp;ГОРОДЕ.
-          </h1>
-        </div>
+          <motion.div className={classes.text} custom={3} variants={variants}>
+            <Title text="КОВОРКИНГ НА ЮГЕ МОСКВЫ" />
+            <motion.h1 custom={4} variants={variants}>
+              САМЫЕ ЛУЧШИЕ КВАДРАТНЫЕ МЕТРЫ В&nbsp;ГОРОДЕ.
+            </motion.h1>
+          </motion.div>
+        </motion.div>
 
         <Cover />
       </div>
