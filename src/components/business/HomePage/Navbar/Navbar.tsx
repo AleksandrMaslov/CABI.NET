56d8286ac6ab7a739c64576cace2402ee6846ea7
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-11 22:17:38 +0300

    refactor: Navbar with separated components

--- src/components/business/HomePage/Navbar/Navbar.tsx
+++ src/components/business/HomePage/Navbar/Navbar.tsx
@@ -8,6 +8,18 @@ interface NavbarProps {
   className?: string
 }
 
+interface NavItemProps {
+  title: string
+  href: string
+  order: number
+}
+
+interface SocialItemProps {
+  icon: 'whatsapp' | 'telegram'
+  href: string
+  order: number
+}
+
 const variants = {
   hidden: { opacity: 0, y: '-5rem' },
 
@@ -18,6 +30,32 @@ const variants = {
   }),
 }
 
+const anchors = [
+  {
+    title: 'УСЛУГИ',
+    href: '#services',
+  },
+  {
+    title: 'ПЛАН КОВОРКИНГА',
+    href: '#plan',
+  },
+  {
+    title: 'КОНТАКТЫ',
+    href: '#contacts',
+  },
+]
+
+const socials: { icon: 'whatsapp' | 'telegram'; href: string }[] = [
+  {
+    icon: 'whatsapp',
+    href: '#',
+  },
+  {
+    icon: 'telegram',
+    href: '#',
+  },
+]
+
 const Navbar: FC<NavbarProps> = ({ className }) => {
   const rootClasses = [classes.navbar]
   if (className) rootClasses.push(className)
@@ -25,36 +63,16 @@ const Navbar: FC<NavbarProps> = ({ className }) => {
   return (
     <nav className={rootClasses.join(' ')}>
       <motion.ul className={classes.anchors}>
-        <motion.li custom={2} variants={variants}>
-          <Anchor href="#services">УСЛУГИ</Anchor>
-        </motion.li>
-        <motion.li custom={3} variants={variants}>
-          <Anchor href="#plan">ПЛАН КОВОРКИНГА</Anchor>
-        </motion.li>
-        <motion.li custom={4} variants={variants}>
-          <Anchor href="#contacts">КОНТАКТЫ</Anchor>
-        </motion.li>
+        {anchors.map((anchor, i) => (
+          <NavItem key={anchor.title} {...anchor} order={i + 2} />
+        ))}
       </motion.ul>
 
       <ul className={classes.actions}>
         <div className={classes.social}>
-          <motion.a
-            href="#"
-            className={classes.icon}
-            custom={5}
-            variants={variants}
-          >
-            <Icon icon="whatsapp" size="3.5rem" />
-          </motion.a>
-
-          <motion.a
-            href="#"
-            className={classes.icon}
-            custom={6}
-            variants={variants}
-          >
-            <Icon icon="telegram" size="3.5rem" />
-          </motion.a>
+          {socials.map((social, i) => (
+            <SocialItem key={social.icon} {...social} order={i + 5} />
+          ))}
         </div>
 
         <motion.span custom={7} variants={variants}>
@@ -66,6 +84,27 @@ const Navbar: FC<NavbarProps> = ({ className }) => {
         </motion.span>
       </ul>
     </nav>
+  )
+}
+
+const NavItem: FC<NavItemProps> = ({ title, href, order }) => {
+  return (
+    <motion.li custom={order} variants={variants}>
+      <Anchor href={href}>{title}</Anchor>
+    </motion.li>
+  )
+}
+
+const SocialItem: FC<SocialItemProps> = ({ icon, href, order }) => {
+  return (
+    <motion.a
+      href={href}
+      custom={order}
+      variants={variants}
+      className={classes.icon}
+    >
+      <Icon icon={icon} size="3.5rem" />
+    </motion.a>
   )
 }
 
