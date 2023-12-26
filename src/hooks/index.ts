ef18f3fbbd7e1b09189e961a358321614ff76e9a
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-26 21:06:14 +0300

    add: useDelayedUnmount hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+export { default as useDelayedUnmount } from './useDelayedUnmount'
 export { default as useInfinitePagination } from './useInfinitePagination'
 export { default as useMediaQuery } from './useMediaQuery'
 export { default as useToggle } from './useToggle'
