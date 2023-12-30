6fbff439b57ba2eb72a9401568bacce84244fd18
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-30 13:24:57 +0300

    add: ServerDummyService send method data logging

--- src/services/ServerDummyService.ts
+++ src/services/ServerDummyService.ts
@@ -1,7 +1,10 @@
+import { IObject } from 'src/models'
 import { delay } from 'src/utils'
 
 export default class ServerDummyService {
-  public static sendApplicationData() {
+  public static sendApplicationData(params?: IObject) {
+    // eslint-disable-next-line no-console
+    console.log('Данные отправлены:', params)
     return delay(1500)
   }
 }
