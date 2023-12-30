bf24762010f549b0e54264669af8fda5a5314fca
AleksandrMaslov <amaslov8910@gmail.com>
2023-12-30 13:11:25 +0300

    refactor: ApplicationForm with validation and data

--- src/components/business/forms/ApplicationForm/ApplicationForm.tsx
+++ src/components/business/forms/ApplicationForm/ApplicationForm.tsx
@@ -1,5 +1,11 @@
 import { Button, Input } from 'cabinet_ui_kit'
-import { FC } from 'react'
+import { FC, FormEventHandler, useContext } from 'react'
+
+import { ModalContext } from 'src/context'
+import { useFetching, useInput } from 'src/hooks'
+import { ServerDummyService } from 'src/services'
+
+import { Confirmation, Warning } from '../..'
 
 import classes from './ApplicationForm.module.css'
 
@@ -8,50 +14,73 @@ interface ApplicationFormProps {
 }
 
 const ApplicationForm: FC<ApplicationFormProps> = ({ className }) => {
+  const { openModal, closeModal } = useContext(ModalContext)
+
   const rootClasses = [classes.applicationForm]
   if (className) rootClasses.push(className)
 
+  const [usernameProps, usernameSettings] = useInput({ isEmpty: true })
+  const [telProps, telSettings] = useInput({ isEmpty: true, isTel: true })
+  const [emailProps, emailSettings] = useInput({ isEmail: true })
+  const [commentsProps] = useInput()
+
+  const [sendData, isLoading, error] = useFetching(async () => {
+    await ServerDummyService.sendApplicationData()
+  })
+
+  const submitHandler: FormEventHandler<HTMLFormElement> = async e => {
+    e.preventDefault()
+    await sendData()
+    await closeModal()
+    if (error) return openModal(<Warning />)
+    openModal(<Confirmation />)
+  }
+
   return (
-    <form className={rootClasses.join(' ')} onSubmit={e => e.preventDefault()}>
+    <form className={rootClasses.join(' ')} onSubmit={submitHandler}>
       <h3 className={classes.title}>Оставить заявку</h3>
 
       <Input
         placeholder="Ваше имя*"
         name="username"
         id="username"
-        // disabled={isLoading}
-        // {...usernameProps}
+        disabled={isLoading}
+        {...usernameProps}
       />
 
       <Input
         placeholder="Номер телефона*"
         name="tel"
         id="tel"
-        // disabled={isLoading}
-        // {...telProps}
+        disabled={isLoading}
+        {...telProps}
       />
 
       <Input
         placeholder="E-mail"
         name="email"
         id="email"
-        // disabled={isLoading}
-        // {...textProps}
+        disabled={isLoading}
+        {...emailProps}
       />
 
       <Input
         placeholder="Комментарии"
         name="comments"
         id="comments"
-        // disabled={isLoading}
-        // {...textProps}
+        disabled={isLoading}
+        {...commentsProps}
       />
 
       <Button
         className={classes.btn}
         label="ОТПРАВИТЬ ЗАЯВКУ"
-        // isLoading={isLoading}
-        // disabled={!usernameSettings.isValid || !telSettings.isValid}
+        isLoading={isLoading}
+        disabled={
+          !usernameSettings.isValid ||
+          !telSettings.isValid ||
+          !emailSettings.isValid
+        }
       />
 
       <p className={classes.note}>
