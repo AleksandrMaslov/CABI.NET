74a3cf3617921adcc33e133d1ef85c18a7f589f0
AleksandrMaslov <amaslov8910@gmail.com>
2024-01-07 20:20:51 +0300

    refactor: useLocalStorage with undefined value

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -3,21 +3,21 @@ import { useState } from 'react'
 export default function useLocalStorage<T>(
   key: string,
   defaultValue?: T,
-): [T | null, (value: T | null) => void, () => void] {
+): [T | undefined, (value?: T) => void, () => void] {
   const getValue = () => {
     const storage = localStorage.getItem(key)
-    if (!storage) return defaultValue || null
+    if (!storage) return defaultValue
     return JSON.parse(storage) as T
   }
 
-  const [value, setValue] = useState<T | null>(getValue)
+  const [value, setValue] = useState<T | undefined>(getValue)
 
   const removeValue = () => {
     localStorage.removeItem(key)
-    setValue(null)
+    setValue(undefined)
   }
 
-  const saveValue = (value: T | null) => {
+  const saveValue = (value?: T) => {
     if (!value) removeValue()
     localStorage.setItem(key, JSON.stringify(value))
     setValue(value)
