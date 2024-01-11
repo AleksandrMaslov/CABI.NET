5bff555896f735088e3bd2bf4e717cb871f59ab1
AleksandrMaslov <amaslov8910@gmail.com>
2024-01-11 22:39:32 +0300

    refactor: router with env vars

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,19 +6,12 @@ import { routes } from './routes'
 
 const isHashRouter = import.meta.env.MODE === 'hash'
 const createRouter = isHashRouter ? createHashRouter : createBrowserRouter
+const basename = isHashRouter ? undefined : import.meta.env.VITE_BASE_URL
+const errorElement = <ErrorRouteElement />
 
 const router = createRouter(
-  routes.map(({ path, element, loader }) => {
-    return {
-      path,
-      element,
-      loader,
-      errorElement: <ErrorRouteElement />,
-    }
-  }),
-  {
-    basename: isHashRouter ? undefined : '/CABI.NET',
-  },
+  routes.map(route => ({ ...route, errorElement })),
+  { basename },
 )
 
 export default router
